ea81ba2002ffb5101c72070399b3855f74a99671
AndreaHolecz <andreaholecz94@gmail.com>
2022-05-09 10:14:42 +0200

    idk

--- frontend/script.js
+++ frontend/script.js
@@ -1,8 +1,8 @@
-
-
+let globalVariable1 = 12;
 
 const loadEvent = function (){
     const rootElement = document.getElementById("root")
+    console.log(globalVariable1);
 }
 
 window.addEventListener("load", loadEvent)
