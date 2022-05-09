91399a956eabcbb0b0a1677f5edede20947036c4
AndreaHolecz <andreaholecz94@gmail.com>
2022-05-09 11:40:20 +0200

    idkwtfisgoingon

--- frontend/script.js
+++ frontend/script.js
@@ -2,26 +2,26 @@ let globalVariable1 = 12;
 
 let globalVariable2 = function () {
     console.log ("inside the function", globalVariable1);
-
+    
     let funVariable1 = "hello"
-
+    
     let funVariable2 = function () {
         console.log("funVariable2 belseje");
         console.log(globalVariable1);
         console.log(funVariable1);
 
         let funInFunVariable1 = true;
-            console.log(funInFunVariable1);
+        console.log(funInFunVariable1);
 
-
+        
         let funInFunVariable2 = function() {
             console.log(funInFunVariable1);
 
         }
-
+        
         funInFunVariable2();
     }
-
+    
     funVariable2();
 
     console.log (funVariable1);
@@ -40,7 +40,10 @@ globalVariable2()
 /*console.log (funvariable1);*/
 
 var varVariable = 1;
+
 console.log(varVariable);
+
+let varVariable = 2;
 
 
 const loadEvent = function (){
