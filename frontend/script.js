159f1ce47c1317de20db8c73c2a858b90dace53a
AndreaHolecz <andreaholecz94@gmail.com>
2022-05-09 10:23:00 +0200

    wtf

--- frontend/script.js
+++ frontend/script.js
@@ -1,7 +1,17 @@
 let globalVariable1 = 12;
 
+let globalVariable2 = function () {
+    console.log ("inside the function", globalVariable1);
+    let funVariable1 = "hello"
+}
+
+globalVariable2()
+
+/*console.log (funvariable1);*/
+
 const loadEvent = function (){
     const rootElement = document.getElementById("root")
+
     console.log(globalVariable1);
 }
 
