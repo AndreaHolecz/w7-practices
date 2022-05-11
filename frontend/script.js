104bf7a5a6423f5b48561991ac0d6602b9091b51
AndreaHolecz <andreaholecz94@gmail.com>
2022-05-11 10:22:24 +0200

    push

--- frontend/script.js
+++ frontend/script.js
@@ -2,21 +2,47 @@ const menuButtonComponent = function() {
     return `<button id="menu-btn"></button>`
 }
 
-const loadEvent = function (parameter1){
+const beerSectionComponent = function(id, title, sub, text) {
+    return `
+        <section id="${id}">
+            <h1 class="beerName">${title}</h1>
+            <h2 class="beerCompany">${sub}</h2>
+            <h3 class="beerType">${text}</h3>
+        </section>
+    `
+}
+
+const beerAnchorComponent = function (id, title) {
+    return `
+        <a href="#${id}">${title}</a>
+    `
+}
+
+const menuButtonClickEvent = function (event) {
+
+    event.currentTarget.closest("#root").classList.toggle("menu-opened")
+}
+
+
+const loadEvent = function (){
+    
+    /*console.log(typeOfBeerSection);
+    console.log(typeOfBeerSection());
+
+    (function () {
+        console.log("blablabla");
+    })();
+    */
+
     const rootElement = document.getElementById("root");
-    console.log(rootElement.parentElement);
 
     rootElement.insertAdjacentHTML("beforeend", menuButtonComponent())
 
     const menuButtonElement = document.getElementById ("menu-btn")
 
     menuButtonElement.addEventListener("click", function (event) {
-        //event.target.classList.toggle("clicked")
 
-        //console.log(rootElement);
-        //console.log(event.currentTarget.parentElement);
-        //console.log(event.currentTarget.closest("#root"));
-        event.currentTarget.closest("#root").classList.toggle("menu-opened");
+        //event.currentTarget.closest("#root").classList.toggle("menu-opened");
     })
 }
 
